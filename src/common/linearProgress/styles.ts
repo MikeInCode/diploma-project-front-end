f7a1b0c6f7ebbff57efddcbdf1dd2eeb547c3cd2
Mike <serbin.michael98@gmail.com>
2019-11-20 20:33:43 +0200

    fix

--- src/common/linearProgress/styles.ts
+++ src/common/linearProgress/styles.ts
@@ -1,11 +1,14 @@
 import { makeStyles } from '@material-ui/core'
 import { Color } from '../../theme'
 
-export const useLinearProgressClasses = makeStyles({
-  root: {
-    background: Color.Green2
+export const useLinearProgressClasses = makeStyles(
+  {
+    root: {
+      background: Color.Green2
+    },
+    bar: {
+      background: Color.Green
+    }
   },
-  bar: {
-    background: Color.Green
-  }
-})
+  { name: 'CustomLinearProgressClasses' }
+)
